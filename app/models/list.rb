9f3383786218c5881b62253f393e5352f7a6ed41
Elisabeth Camara <ecamara.k@gmail.com>
2025-03-14 15:56:16 +0100

    lists index

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :items, through: :bookmarks
+  has_one_attached :photo
 
   validates :name, presence: true
 end
